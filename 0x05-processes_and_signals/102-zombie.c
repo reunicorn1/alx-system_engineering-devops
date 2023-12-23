9425031312b865a9d1d9257c15984c1d862e04d2
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2023-12-23 04:43:46 +0400

    102-zombie.c

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/*
+ * infinite_while - create an infinite while loop
+ *
+ * Return: Always 0.
+ */
+
+int infinite_while(void)
+{
+    while (1)
+    {
+        sleep(1);
+    }
+    return (0);
+}
+
+/*
+ * main - creates zombie processes
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+	pid_t pid = 1;
+	for (i = 0; i < 5; i++)
+	{
+		if (pid > 0)
+			pid = fork();
+		if (pid == 0)
+		{
+			printf("Zombie process created, PID: %i\n", getpid());
+			exit(0);
+		}
+	}
+	infinite_while();
+}

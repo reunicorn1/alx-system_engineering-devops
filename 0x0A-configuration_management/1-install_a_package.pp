0fa658e5785ac474ee2c0f60bb33c4094509d4c0
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-02-24 06:10:29 +0400

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-#!/usr/bin//pup
 # This Puppet installs flast from pip3
-package {'flask':
-	ensure => '2.1.0',
-	provider => 'pip3'
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/bin', '/usr/bin'],
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
 }

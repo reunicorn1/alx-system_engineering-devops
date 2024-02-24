71204c05a596e056cb1d5b8819413f4f5c8cbd95
Reem Osama <ireosama1@gmail.com>
2024-02-23 18:46:53 -0800

    2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,4 @@
- # This Puppet kills a process named killmeow
+# This Puppet kills a process named killmenow
 exec { 'kill_killmenow_process':
   command     => '/usr/bin/pkill killmenow',
   refreshonly => true,

8eb8811fdb61ef52616a55e5ef975784917ddfc8
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-02-26 06:57:35 +0400

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,27 +1,15 @@
 # Puppet manifest for a configuration file in the server
 include stdlib
-$home_dir = $facts['home']
 
-file { "${home_dir}/.ssh":
-  ensure => directory,
-  mode   => '0700',
-}
-file { "${home_dir}/.ssh/config":
-  ensure  => file,
-  mode    => '0600',
-  content => "
-    Host *
-      IdentityFile ~/.ssh/school
-      PasswordAuthentication no
-  ",
-}
 file_line { 'Turn off passwd auth':
-  ensure => present,
-  path   => "${home_dir}/.ssh/config",
-  line   => 'PasswordAuthentication no',
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
   }
 file_line { 'Declare identity file':
-  ensure => presnet,
-  path   => "${home_dir}/.ssh/config",
-  line   => 'IdentityFile ~/.ssh/school',
+  ensure  => presnet,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/school',
+  replace => true,
 }

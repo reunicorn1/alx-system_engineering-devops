e5f1e08a6c491e194a7d55bbffa09c2e56e9b0ef
reunicorn1 <ireosama1@gmail.com>
2024-02-25 18:48:02 -0800

    100-puppet_ssh_config.pp adding the file first

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,27 @@
 # Puppet manifest for a configuration file
 include stdlib
+$home_dir = $facts['home']
 
+file { "${home_dir}/.ssh":
+  ensure  => directory,
+  mode    => '0700',
+}
+file { "${home_dir}/.ssh/config":
+  ensure  => file,
+  mode    => '0600',
+  content => "
+    Host *
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  ",
+}
 file_line { 'Turn off passwd auth':
-  path   => '/etc/ssh/sshd_config',
+  path   => "${home_dir}/.ssh/config",
   line   => 'PasswordAuthentication no',
   ensure => present,
 }
 file_line { 'Declare identity file':
-  path   => '/etc/ssh/sshd_config',
+  path   => "${home_dir}/.ssh/config",
   line   => 'IdentityFile ~/.ssh/school',
   ensure => present,
 }

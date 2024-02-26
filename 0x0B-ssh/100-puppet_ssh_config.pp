c771faa066a99a0108db8134df6cb534a9875869
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-02-26 06:53:07 +0400

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,8 +3,8 @@ include stdlib
 $home_dir = $facts['home']
 
 file { "${home_dir}/.ssh":
-  ensure  => directory,
-  mode    => '0700',
+  ensure => directory,
+  mode   => '0700',
 }
 file { "${home_dir}/.ssh/config":
   ensure  => file,
@@ -16,11 +16,12 @@ file { "${home_dir}/.ssh/config":
   ",
 }
 file_line { 'Turn off passwd auth':
+  ensure => present,
   path   => "${home_dir}/.ssh/config",
   line   => 'PasswordAuthentication no',
-  ensure => present,
-}
+  }
 file_line { 'Declare identity file':
+  ensure => presnet,
   path   => "${home_dir}/.ssh/config",
   line   => 'IdentityFile ~/.ssh/school',
   ensure => present,

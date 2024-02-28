231006dfb73c98a2f4b496017df544c7afce4264
reunicorn1 <ireosama1@gmail.com>
2024-02-27 17:41:00 -0800

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,25 +1,7 @@
 # Puppet manifest for a configuration file in the server
 
-# execute 'apt-get update'
-exec { 'apt-update':
-  command => '/usr/bin/apt-get update'
-}
-
-# install nginx package
-package { 'nginx':
-  ensure  => installed
-  require => Exec['apt-update'],
-}
-
-# hello world HTML file
-file { '/var/www/htmlindex.html':
-  ensure  => file,
-  content => 'Hello World!',
-  require => Package['nginx']
-}
-
 # replacing lines for the redirection
 exec {'configure':
   provider => shell,
-  command  => 'sudo sed -i "s#server_name _;#server_name radientrider.tech;\n        location /redirect_me {\n                rewrite ^/redirect_me/?$ https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;\n        }#" /etc/nginx/sites-available/default ; sudo service nginx restart',
+  command  => 'sudo apt-get update -y ; sudo apt-get install nginx -y ; echo "Hello World!" | sudo tee /var/www/html/index.html ; sudo sed -i "s#server_name _;#server_name radientrider.tech;\n        location /redirect_me {\n                rewrite ^/redirect_me/?$ https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;\n        }#" /etc/nginx/sites-available/default ; sudo service nginx restart',
 }

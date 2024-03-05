b57114d0167ae9265c30a2e2fc81ef5df94c7b1e
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-03-05 18:32:51 +0400

    2-puppet_custom_http_response_header.pp - mod

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,6 +1,30 @@
 # Puppet manifest for a configuration file in the server
-exec {'configure':
-  provider => shell,
-  command  => 'sudo apt-get update -y ; sudo apt-get install nginx -y ; echo "Hello World!" | sudo tee /var/www/html/index.html ; sudo sed -i "s#server_name _;#server_name radientrider.tech;\n        location /redirect_me {\n                rewrite ^/redirect_me/?$ https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;\n        }#" /etc/nginx/sites-available/default ; sudo service nginx restart ; hostname=$(hostname) ; string="\n\tadd_header X-Served-By \"$hostname\";" ; sudo sed -i "/server_name _;/ a\\
-$string" /etc/nginx/sites-available/default ; sudo service nginx restart',
+# execute 'apt-get update'
+exec { 'apt-update':
+	command => '/usr/bin/apt-get update'
+}
+
+# install nginx package
+package { 'nginx':
+	require => Exec['apt-update'],
+	ensure  => installed,
+}
+
+# hello world HTML file
+file { '/var/www/htmlindex.html':
+	ensure  => file,
+	content => 'Hello World!',
+	require => Package['nginx']
+}
+
+# replacing lines for the redirection
+exec { 'Redirection':
+	provider => shell,
+	command  => 'sudo sed -i "s#server_name _;#server_name _;\n        location /redirect_me {\n                rewrite ^/redirect_me/?$ https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;\n        }#" /etc/nginx/sites-available/default ; sudo service nginx restart',
+}
+
+# custom header creation 
+exec { 'custom_header':
+	provider => shell,
+	command  => 'hostname=$(hostname) ; string="\tadd_header X-Served-By \"$hostname\";" ; sudo sed -i "/server_name _;/ a\\ $string\n" /etc/nginx/sites-available/default ; sudo service nginx restart',
 }

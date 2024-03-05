7ca662e803b92d9c291f2d9243b58d81d46e10ff
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-03-05 07:16:50 +0400

    1-install_load_balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,6 +1,4 @@
 # Puppet manifest for a configuration file in the server
-
-# replacing lines for the redirection
 exec {'configure':
   provider => shell,
   command  => 'sudo apt-get update -y ; sudo apt-get install nginx -y ; echo "Hello World!" | sudo tee /var/www/html/index.html ; sudo sed -i "s#server_name _;#server_name radientrider.tech;\n        location /redirect_me {\n                rewrite ^/redirect_me/?$ https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;\n        }#" /etc/nginx/sites-available/default ; sudo service nginx restart ; hostname=$(hostname) ; string="\n\tadd_header X-Served-By \"$hostname\";" ; sudo sed -i "/server_name _;/ a\\

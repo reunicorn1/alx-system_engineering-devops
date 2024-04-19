9ee2714324efd76d33cfa5384e0a9c6c87ccb728
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-04-19 06:03:13 +0400

    1-user_limit.pp - a long line

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,6 @@
 # Puppet manifest to modify the user limit
 exec{ 'modify limit':
   provider => shell,
-  command  => 'sudo sed -i "s/holberton hard nofile 5/holberton hard nofile 65536/" /etc/security/limits.conf && sed -i "s/holberton soft nofile 4/holberton soft nofile 65536/" /etc/security/limits.conf',
+  command  => 'sudo sed -i "s/holberton hard nofile 5/holberton hard nofile 65536/" /etc/security/limits.conf && 
+  sed -i "s/holberton soft nofile 4/holberton soft nofile 65536/" /etc/security/limits.conf',
 }
